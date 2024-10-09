13b5d4540a5f9fa50aa17746ab5ccab2e205b21e
BuildTools <unconfigured@null.spigotmc.org>
2024-10-09 07:57:58 +0200

    Initial Commit

---
+++ Seen/SeenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeenApp.swift
+//  Seen
+//
+//  Created by cutiepie on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SeenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
